dbb5dc4df50bce5e864626e10fc5151ba5fabd05
Damiano Giusti <damianogiusti95@gmail.com>
2017-09-20 22:41:52 +0200

    Refactor MainPresenter

--- app/src/main/java/com/damianogiusti/cleanandroid/ui/home/MainPresenter.java
+++ app/src/main/java/com/damianogiusti/cleanandroid/ui/home/MainPresenter.java
@@ -1,6 +1,6 @@
 package com.damianogiusti.cleanandroid.ui.home;
 
-import android.os.Bundle;
+import android.util.Log;
 
 import com.damianogiusti.cleanandroid.data.datamodel.ProvinceDataModel;
 import com.damianogiusti.cleanandroid.mvp.Presenter;
@@ -8,54 +8,52 @@ import com.damianogiusti.cleanandroid.viewmodel.ProvinceViewModel;
 import com.damianogiusti.domain.cleanandroid.interactors.GetProvinceListUseCase;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-import javax.inject.Inject;
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
 
 /**
  * Created by Damiano Giusti on 03/05/17.
  */
 public class MainPresenter implements Presenter<MainView> {
 
+    private static final String TAG = "MainPresenter";
+
     private MainView mainView;
 
-    private GetProvinceListUseCase getProvinceListUseCase;
+    private Observable<List<ProvinceViewModel>> observableProvinces;
 
-    @Inject
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
     public MainPresenter(GetProvinceListUseCase getProvinceListUseCase) {
-        this.getProvinceListUseCase = getProvinceListUseCase;
+        observableProvinces = getProvinceListUseCase.use()
+                .map(this::mapToViewModel)      // convert the DataModel to ViewModel
+                .share()
+                .replay(1)
+                .autoConnect();
+
+        Log.d(TAG, "MainPresenter: created " + this);
     }
 
     @Override
-    public void create(MainView view, Bundle params) {
+    public void attachView(MainView view) {
         mainView = view;
-    }
 
-    @Override
-    public void resume() {
+        Log.d(TAG, "attachView into presenter " + this);
 
-        // do this on every resume for example purposes, to visually check the origin of data
-        // slow -> REST
-        // fast -> Memory Cache
-
-        mainView.showLoading();
-
-        getProvinceListUseCase.use()
-                .map(this::mapToViewModel)              // convert the DataModel to ViewModel
+        observableProvinces
+                .doOnSubscribe(compositeDisposable::add)
                 .doOnTerminate(mainView::hideLoading)   // dismiss the dialog on terminate
                 .subscribe(this::onProvinceListReceived, this::onError);
     }
 
     @Override
-    public void pause() {
+    public void detachView() {
+        compositeDisposable.clear();
 
-    }
-
-    @Override
-    public void destroy() {
-        mainView = null;
-        // free the resources occupied by the use case
-        getProvinceListUseCase.unsubscribe();
+        Log.d(TAG, "detachView from presenter " + this);
     }
 
     private void onProvinceListReceived(List<ProvinceViewModel> provinceList) {
@@ -71,7 +69,7 @@ public class MainPresenter implements Presenter<MainView> {
      * For example, your business logic models might not be appropriate for showing them to the user directly.
      * Perhaps you need to show a combination of multiple business logic models at once.
      */
-    private List<ProvinceViewModel> mapToViewModel(List<ProvinceDataModel> provinceDataModels) {
+    private List<ProvinceViewModel> mapToViewModel(Collection<ProvinceDataModel> provinceDataModels) {
         List<ProvinceViewModel> provinceViewModels = new ArrayList<>(provinceDataModels.size());
         for (ProvinceDataModel provinceDataModel : provinceDataModels) {
             provinceViewModels.add(new ProvinceViewModel(provinceDataModel.getSigla(), provinceDataModel.getDescrizione()));
